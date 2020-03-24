2466ce5bfd2c30e8b04cd66e92b70c28806e8e9b
Vinicius Consulmagnos Romeiro <vinicius.romeiro@gmail.com>
2020-03-24 16:13:19 -0300

    aprendendo swift protocol

---
+++ Capacitacao_Protocol.playground/Pages/Desafio.xcplaygroundpage/Contents.swift
@@ -0,0 +1,36 @@
+/*:
+ 
+ # Desafio
+ 
+ */
+
+import UIKit
+
+/*:
+ + Transformar as animações em protocolos utilizando Programação Orientada a Protocolo
+ 
+ + Todos os componentes da tela devem ter o efeito de buzz, além disso a mensagem de erro deve ter também o efeito de pop
+ 
+ + Animar os componentes no click do botão
+ 
+ Sugestão de implementação:
+ 
+ - Implementar o protocolo de animação Buzz
+ - Herdar o TextField adotando o protocolo de Buzz
+ - Plugar o protocolo do buzz criado no TextField
+ - Implementar o protocolo de animação Pop
+ - Herdar o UILabel adotando o protocolo de Pop
+ - Plugar o protocolo do pop criado na label vermelha
+ - Herdar os componentes adotando os protocolos Buzz e Pop
+ - Plugar o protocolo do buzz e do pop nos outlets
+ 
+ Obs: *Plugar = alterar tipo da classe no storyboard/outlet*
+ 
+ */
+//: [Código fonte do desafio POP](https://github.com/Vinicius Romeiro/SwiftProtocol/tree/master/challenge/protocolChallenge)
+
+
+/*:
+ ****
+ [Previous](@previous) | [Next](@next)
+ */
